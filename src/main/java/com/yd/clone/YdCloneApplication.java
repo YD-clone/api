fd2a2e7da124ce6e5ecc5fd219817b77ca99c83d
chaejinho <asdzxc9395@naver.com>
2020-09-09 16:52:35 +0900

    setting files

---
+++ src/main/java/com/yd/clone/YdCloneApplication.java
@@ -0,0 +1,13 @@
+package com.yd.clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YdCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YdCloneApplication.class, args);
+	}
+
+}
